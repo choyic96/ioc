450f70a061de99a472a836cd53aba8f073ab995c
choyic96 <choyic96@naver.com>
2021-12-29 12:01:34 +0900

    IOC 도입 전 개발자가 일일히 생성자단에서 생성할 객체 타입을 변경해서 객체 생성해야 함

--- src/kr/co/ezenac/Ioc/Main.java
+++ src/kr/co/ezenac/Ioc/Main.java
@@ -7,15 +7,8 @@ public class Main {
 
         String url = "www.google.com/books/it?page=1000&name=springframework";
 
-        //Base64 encoding
-        IEncoder encoder = new Base64Encoder();
+        Encoder encoder = new Encoder();
         String result = encoder.encode(url);
         System.out.println(result);
-
-        //url encoding
-        IEncoder urlEncoder = new UrlEncoder();
-        String urlResult = urlEncoder.encode(url);
-        System.out.println(urlResult);
-
     }
 }
